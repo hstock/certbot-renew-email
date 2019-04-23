855c123635c9246885b99e60a77007dda872faa8
Hanno Stock <opensource@hanno-stock.de>
2019-04-23 14:28:31 +0200

    Move message creation to separate method

--- certbot_notify_post_hook.py
+++ certbot_notify_post_hook.py
@@ -11,23 +11,7 @@ from io import StringIO
 from argparse import ArgumentParser
 
 
-def main():
-    parser = ArgumentParser(description="Send email notification about renewed certificates.")
-    parser.add_argument('mail_from')
-    parser.add_argument('mail_to')
-    parser.add_argument('--print-only', action='store_true', default=False,
-                        help="Only print the email to stdout; do not send anything")
-    parser.add_argument('--smtp-host', default="localhost", help="SMTP host for sending mails")
-    parser.add_argument('--smtp-port', default="25", type=int, help="SMTP port for sending mails")
-
-    options = parser.parse_args()
-
-    mail_from = options.mail_from
-    mail_to = options.mail_to
-
-    domains = os.environ['RENEWED_DOMAINS'].split()
-    cert_dir = Path(os.environ['RENEWED_LINEAGE'])
-
+def create_message(cert_dir, domains, mail_from, mail_to):
     msg = MIMEMultipart()
     msg['Subject'] = 'Certificate Update for {}'.format(', '.join(domains))
     msg['From'] = mail_from
@@ -45,6 +29,28 @@ def main():
         attachment = MIMEApplication(cert.read(), "x-pem-file", email.encoders.encode_7or8bit)
         attachment["Content-Disposition"] = "attachment; filename=cert.pem"
         msg.attach(attachment)
+
+    return msg
+
+
+def main():
+    parser = ArgumentParser(description="Send email notification about renewed certificates.")
+    parser.add_argument('mail_from')
+    parser.add_argument('mail_to')
+    parser.add_argument('--print-only', action='store_true', default=False,
+                        help="Only print the email to stdout; do not send anything")
+    parser.add_argument('--smtp-host', default="localhost", help="SMTP host for sending mails")
+    parser.add_argument('--smtp-port', default="25", type=int, help="SMTP port for sending mails")
+
+    options = parser.parse_args()
+
+    mail_from = options.mail_from
+    mail_to = options.mail_to
+
+    domains = os.environ['RENEWED_DOMAINS'].split()
+    cert_dir = Path(os.environ['RENEWED_LINEAGE'])
+
+    msg = create_message(cert_dir, domains, mail_from, mail_to)
 
     if options.print_only:
         sys.stdout.buffer.write(msg.as_bytes())
